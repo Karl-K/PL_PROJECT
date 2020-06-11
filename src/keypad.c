f2bc2974e9802636a4292657a06b4aebd7eabfba
Karl-K <49514548+Karl-K@users.noreply.github.com>
2020-06-11 17:12:55 +0900

    2

--- src/keypad.c
+++ src/keypad.c
@@ -31,7 +31,7 @@ int keyboard_read(int * key_value) {
 			for( j=0; j<16; j++){
 				if(buf[i] == keyboard[j]){
 					a = a + j * 10 ^ (key_count - 1 -i);
-					*key_value = j;
+					*key_value = a;
 				}
 			}
 		}
--- src/main.c
+++ src/main.c
@@ -129,7 +129,7 @@ void select_mode() {
 }
 
 void input_mode() {
-	int key_value, i, j, a;
+	int key_value, i, j;
 	char clcd_str1[20];
 	char clcd_str2[20];
 	sprintf( clcd_str1, "People : ");
@@ -140,14 +140,14 @@ void input_mode() {
 	clcd_write_string( clcd_str2 );
 	
 	i = keyboard_read( &key_value );
-	people = a;
+	people = key_value;
 	dot_write( people );
 	sprintf( clcd_str1, "People : %d", people );
 	clcd_set_DDRAM( 0x00 );
 	clcd_write_string( clcd_str1 );
 	
 	j = keyboard_read( &key_value );
-	money = a;
+	money = key_value;
 	fnd_write( money, 7 );
 	sprintf( clcd_str2, "Money : %d", money );
 	clcd_set_DDRAM( 0x40 );
